2d33f3aa1370a6e293242086e635523e3bbfe084
odinahka <odinaldo22@gmail.com>
2022-12-14 14:45:00 +0100

    wallet react code

--- src/About5.js
+++ src/About5.js
@@ -3,9 +3,9 @@ import React from 'react';
 const About5 = () => {
     return (
         <div className='h-screen w-screen bg-wallet_black'>
-            <div className='h-4 w-screen bg-white'/>
+            <div className='h-8 w-screen bg-white'/>
             <div className='container text-white flex flex-col items-stretch justify-between'>
-                <div className='mt-10 pt-20'>
+                <div className='mt-10 pt-12'>
                     <h2 className='opacity-60'>
                         What our customers are saying...
                     </h2>
